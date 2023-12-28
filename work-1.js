978c559611d59d1532631191315ecabf8e2f52c3
Adarsh <adarshshanu3@gmail.com>
2023-12-28 23:56:17 +0530

    Second commit

--- work-1.js
+++ work-1.js
@@ -7,12 +7,17 @@ class ListNode {
 class LinkedList {
     constructor(head = null) {
         this.head = head
+        console.log(this.head)
     }
 }
 let node1 = new ListNode(2)
-let node2 = new ListNode(5)
+let node2 = new ListNode(53)
+let node3 = new ListNode(500)
+let node4 = new ListNode(30)
 node1.next = node2
+node2.next = node3
+node3.next = node4
 
-let list = new LinkedList(node1)
+let list = new LinkedList(node3)
 
-console.log(list.head.next.data) //returns 5
+console.log(list.head.next.data) 
