ed422f503b89b8730e5d948d2029e3fb34e22e5c
Adarsh <adarshshanu3@gmail.com>
2024-01-01 14:25:58 +0530

    singly linked list

--- linkedList.js
+++ linkedList.js
@@ -55,6 +55,59 @@ class LinkedList {
             this.size++
         }
     }
+    removeFrom(index) {
+        if (index < 0 || index > this.size) {
+            return null;
+        }
+        let removeNode;
+        if (index === 0) {
+            removeNode = this.head
+            this.head = this.head.next;
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removeNode = prev.next
+            prev.next = removeNode.next
+        }
+        this.size--;
+        return removeNode.value;
+    }
+    removeValue(value) {
+        if (this.isEmpty()) {
+            return null;
+        }
+        if (this.head.value === value) {
+            this.head = this.head.next
+            this.size--
+            return value
+        } else {
+            let prev = this.head
+            while (prev.next && prev.next.value !== value) {
+                prev = prev.next
+            }
+            let removedNode;
+            if (prev.next) {
+                removedNode = prev.next
+                prev.next = removedNode.next
+                this.size--
+                return value
+            }
+            return null
+        }
+    }
+    reverse() {
+        let prev = null
+        let curr = this.head
+        while (curr) {
+            let next = curr.next
+            curr.next=prev
+            prev = curr
+            curr = next
+        }
+        this.head = prev;
+    }
     print() {
         if (this.isEmpty()) {
             console.log('List is Empty');
@@ -81,5 +134,8 @@ list.prepend(90)
 list.append(100)
 list.append(2)
 list.prepend(2)
-list.insert(500,1)
+list.insert(500, 1)
+console.log(list.removeFrom(0))
+list.removeValue(100)
+list.reverse()
 list.print()
