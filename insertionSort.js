d1d5444861f08af6f4cfe1bf073b94338b12eab4
Adarsh <adarshshanu3@gmail.com>
2024-01-05 19:50:19 +0530

    Quick sorting

--- insertionSort.js
+++ insertionSort.js
@@ -1,17 +1,19 @@
-
+//INSERTION SORT
 
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        let numToSort = arr[i];
+        let numToInsert = arr[i]
         let j = i - 1;
-        while (j >= 0 && arr[j] > numToSort) {
+        while (j >= 0 && arr[j] > numToInsert) {
             arr[j + 1] = arr[j]
-            j = j - 1
+            j--;
         }
-        arr[j + 1] = numToSort;
+        arr[j + 1] = numToInsert;
     }
 }
 
-let arr = [8,20,-2,4,-6]
+let arr = [8, 20, -2, 4, -6,-23]
 insertionSort(arr)
 console.log(arr)
+
+//Big O  O(n*2);
