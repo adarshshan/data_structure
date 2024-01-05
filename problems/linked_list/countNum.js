8f2f52aa83da6100220d0a0311797df67946c7ea
Adarsh <adarshshanu3@gmail.com>
2024-01-06 00:19:02 +0530

    counts the number of times a given integer

---
+++ problems/linked_list/countNum.js
@@ -0,0 +1,66 @@
+class Node {
+    constructor(value) {
+        this.data = value;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    isEmpty() {
+        return this.size === 0;
+    }
+    getSize() {
+        return this.size
+    }
+    append(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+            while (curr.next) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+        this.size++;
+    }
+    print() {
+        if (this.isEmpty()) return console.log(`List is Empty!`);
+        let list = ``;
+        let curr = this.head;
+        while (curr) {
+            list += `${curr.data} `;
+            if (!curr.next) break;
+            curr = curr.next;
+        }
+        console.log(list);
+    }
+}
+
+const obj = new LinkedList()
+obj.append(3)
+obj.append(5)
+obj.append(7)
+obj.append(8)
+obj.append(3)
+obj.append(3)
+obj.append(3)
+obj.append(5)
+obj.append(3)
+obj.print()
+// console.log(obj.getSize())
+function ElementCount(key){
+    let count=0;
+    let len=obj.getSize();
+    let curr=obj.head;
+    for(let i=0;i<len;i++){
+        if(curr.data===key)count++;
+        curr=curr.next;
+    }
+    return count;
+}
+console.log(`Result is ${ElementCount(3)}`)
