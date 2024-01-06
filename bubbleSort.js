cc3565357f7fca15aca78924776d115f966de4f8
Adarsh <adarshshanu3@gmail.com>
2024-01-06 16:03:04 +0530

    Cartition product

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,21 +1,36 @@
-
-function fun(arr) {
-    let flag;
-    do {
-        flag=0;
-        for(let i=0;i<arr.length;i++){
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i]
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                flag = 1;
-            }
-        }
-    } while (flag ===1);
-    return arr;
-}
+//To Sort an array
+// function fun(arr) {
+//     let flag;
+//     do {
+//         flag=0;
+//         for(let i=0;i<arr.length;i++){
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 flag = 1;
+//             }
+//         }
+//     } while (flag ===1);
+//     return arr;
+// }
 
 let arr = [-6, 20, 8, -2, 4]
 fun(arr);
 console.log(arr)
 // Big O  => O(n*2)
+
+
+
+function fun(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                let temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j--;
+            }
+        }
+    }
+}
--- circularLinkedList.js
+++ circularLinkedList.js
@@ -42,8 +42,35 @@ class CircularLinkedList {
         }
         this.size++;
     }
-    isCircular(){
-        return this.tail.next!==null;
+    delete(value) {
+        let curr = this.head;
+        let prev
+        do {
+            prev = curr;
+            curr = curr.next;
+        } while (curr !== this.head && curr.data !== value)
+        if (curr.data === value) {
+            prev.next = curr.next;
+            this.size--;
+        } else {
+            console.log('Entered Number is not exist in linked list!')
+        }
+    }
+    insert(index, value) {
+        const newNode = new Node(value)
+        if (this.isEmpty()) return console.log('list is empty!');
+        let prev = this.head
+        let curr;
+        for (let i = 1; i < index - 1; i++) {
+            prev = prev.next
+            curr = prev.next
+        }
+        prev.next = newNode;
+        newNode.next = curr
+        this.size++;
+    }
+    isCircular() {
+        return this.tail.next !== null;
     }
     display() {
         if (this.isEmpty()) return console.log(`The circular linked list is Empty!`)
@@ -57,11 +84,12 @@ class CircularLinkedList {
     }
 }
 
-const obj=new CircularLinkedList()
+const obj = new CircularLinkedList()
 obj.append(23);
 obj.append(5);
 obj.append(90);
 obj.prepend(40)
 obj.display()
+obj.insert(2,99)
+obj.display()
 console.log(obj.getSize())
-console.log(obj.isCircular())
