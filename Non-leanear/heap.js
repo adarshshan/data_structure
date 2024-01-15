ff7338d2938e33ba7342e660a6477e2769120fdd
Adarsh <adarshshanu3@gmail.com>
2024-01-15 22:30:12 +0530

    MinHeap completed

--- Non-leanear/heap.js
+++ Non-leanear/heap.js
@@ -1,106 +1,99 @@
 class MinHeap {
-	constructor() {
-		this.heap = [];
-	}
-	getLeftChildIndex(parentIndex) {
-		return 2 * parentIndex + 1;
-	}
-	getRightChildIndex(parentIndex) {
-		return 2 * parentIndex + 2;
-	}
-	getParentIndex(childIndex) {
-		return Math.floor((childIndex - 1) / 2);
-	}
-	hasLeftChild(index) {
-		return this.getLeftChildIndex(index) < this.heap.length;
-	}
-	hasRightChild(index) {
-		return this.getRightChildIndex(index) < this.heap.length;
-	}
-	hasParent(index) {
-		return this.getParentIndex(index) >= 0;
-	}
-	leftChild(index) {
-		return this.heap[this.getLeftChildIndex(index)];
-	}
-	rightChild(index) {
-		return this.heap[this.getRightChildIndex(index)];
-	}
-	parent(index) {
-		return this.heap[this.getParentIndex(index)];
-	}
-
-	// Functions to create Min Heap
-	
-	swap(indexOne, indexTwo) {
-		const temp = this.heap[indexOne];
-		this.heap[indexOne] = this.heap[indexTwo];
-		this.heap[indexTwo] = temp;
-	}
-
-	peek() {
-		if (this.heap.length === 0) {
-			return null;
-		}
-		return this.heap[0];
-	}
-	remove() {
-		if (this.heap.length === 0) {
-			return null;
-		}
-		const item = this.heap[0];
-		this.heap[0] = this.heap[this.heap.length - 1];
-		this.heap.pop();
-		this.heapifyDown();
-		return item;
-	}
-
-	add(item) {
-		this.heap.push(item);
-		this.heapifyUp();
-	}
-
-	heapifyUp() {
-		let index = this.heap.length - 1;
-		while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-			this.swap(this.getParentIndex(index), index);
-			index = this.getParentIndex(index);
-		}
-	}
-
-	heapifyDown() {
-		let index = 0;
-		while (this.hasLeftChild(index)) {
-			let smallerChildIndex = this.getLeftChildIndex(index);
-			if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
-				smallerChildIndex = this.getRightChildIndex(index);
-			}
-			if (this.heap[index] < this.heap[smallerChildIndex]) {
-				break;
-			} else {
-				this.swap(index, smallerChildIndex);
-			}
-			index = smallerChildIndex;
-		}
-	}
-	printHeap() {
-		var heap =` ${this.heap[0]} `
-		for(var i = 1; i<this.heap.length;i++) {
-			heap += ` ${this.heap[i]} `;
-		}
-		console.log(heap);
-	}
+    constructor() {
+        this.heap = [];
+    }
+    add(item) {
+        this.heap.push(item);
+        this.heapifyUp();
+    }
+    remove() {
+        if (this.heap.length === 0) return null;
+        const item=this.heap[0];
+        this.heap[0]=this.heap[this.heap.length-1];
+        this.heap.pop();
+        this.heapifyDown();
+        return item;
+    }
+    heapifyDown(){
+        let index=0;
+        while(this.hasLeftChild(index)){
+            let smallChild=this.getLeftChild(index);
+            if(this.hasRightChild(index)&&this.rightChildValue(index)<this.leftChildValue(index)){
+                smallChild=this.getRightChild(index);
+            }
+            if(this.heap[index]<this.heap[smallChild])break;
+            else this.swap(index,smallChild);
+            index=smallChild;
+        }
+    }
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (this.hasParant(index) && this.heap[this.getParant(index)] > this.heap[index]) {
+            this.swap(this.getParant(index), index);
+            index = this.getParant(index);
+        }
+    }
+    swap(parantIndex, childIndex) {
+        let temp = this.heap[parantIndex];
+        this.heap[parantIndex] = this.heap[childIndex];
+        this.heap[childIndex] = temp;
+    }
+    getLeftChild(index) {
+        return (2 * index) + 1;
+    }
+    getRightChild(index) {
+        return (2 * index) + 2;
+    }
+    getParant(index) {
+        return Math.floor((index - 1) / 2);
+    }
+    hasLeftChild(index) {
+        return this.getLeftChild(index) < this.heap.length;
+    }
+    hasRightChild(index) {
+        return this.getRightChild(index) < this.heap.length;
+    }
+    hasParant(index) {
+        return this.getParant(index) >= 0;
+    }
+    leftChildValue(index) {
+        return this.heap[this.getLeftChild(index)];
+    }
+    rightChildValue(index) {
+        return this.heap[this.getRightChild(index)];
+    }
+    parantValue(index) {
+        return this.heap[this.getParant(index)];
+    }
+    peek() {
+        if (this.heap.length === 0) return null;
+        return this.heap[0];
+    }
+    print(){
+        if(this.heap.length===0)return null;
+        let list=``;
+        for(let i=0;i<this.heap.length;i++){
+            list+=`${this.heap[i]} `;
+        }
+        console.log(list);
+    }
 }
-var heap = new MinHeap();
-heap.add(10);
-heap.add(15);
-heap.add(30);
-heap.add(40);
-heap.add(50);
-heap.add(100);
-heap.add(40);
-heap.printHeap();
-console.log(heap.peek());
-console.log(heap.remove());
-// After Deletion.
-heap.printHeap();
+
+const heap=new MinHeap()
+heap.add(22)
+heap.add(18)
+heap.add(26)
+heap.add(34)
+heap.add(40)
+heap.add(45)
+heap.add(8)
+heap.add(10)
+heap.add(15)
+heap.add(20)
+heap.add(25)
+heap.print();
+console.log(heap.peek())
+console.log(`Deleted ${heap.remove()}`);
+console.log(`Deleted ${heap.remove()}`);
+heap.print()
+
