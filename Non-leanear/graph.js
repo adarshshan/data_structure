d556bf9e6b0638a2f263ea8aeabdabc18c7ac74a
Adarsh <adarshshanu3@gmail.com>
2024-01-17 17:22:54 +0530

    graph using Map

--- Non-leanear/graph.js
+++ Non-leanear/graph.js
@@ -1,56 +1,50 @@
-class Graph{
+class Graph {
     constructor() {
         this.adjacencyList = {}
     }
     addNode(node) {
-        if (!this.adjacencyList[node]) this.adjacencyList[node] = []
+        if (!this.adjacencyList[node]) this.adjacencyList[node] = [];
     }
-    addConnection(node1,node2) {
-        this.adjacencyList[node1].push(node2)
-        this.adjacencyList[node2].push(node1)
+    addConnection(node1, node2) {
+        if (!this.hasNode(node1)) this.addNode(node1)
+        if (!this.hasNode(node2)) this.addNode(node2);
+        this.adjacencyList[node1].push(node2);
+        this.adjacencyList[node2].push(node1);
     }
-    removeConnection(node1,node2) {
-        this.adjacencyList[node1] = this.adjacencyList[node1].filter(v => v !== node2)
-        this.adjacencyList[node2] = this.adjacencyList[node2].filter(v => v !== node1)
-    }
-    removeNode(node){
-        while(this.adjacencyList[node].length) {
-            const adjacentNode = this.adjacencyList[node].pop()
-            this.removeConnection(node, adjacentNode)
+    removeNode(node) {
+        while (this.adjacencyList[node].length) {
+            const adjacentNode = this.adjacencyList[node].pop();
+            this.removeConnection(node, adjacentNode);
         }
-        delete this.adjacencyList[node]
+        delete this.adjacencyList[node];
+    }
+    removeConnection(node1, node2) {
+        this.adjacencyList[node1] = this.adjacencyList[node1].filter(item => item !== node2);
+        this.adjacencyList[node2] = this.adjacencyList[node2].filter(item => item !== node1);
+    }
+    hasNode(node) {
+        return this.adjacencyList[node] ? true : false;
+    }
+    printGraph(){
+        let keys=this.adjacencyList.keys();
+        for(let v of keys){
+            let eList=this.adjacencyList.get(v);
+            let data='';
+            for(let e in eList){
+                data+=eList[e]+' '; 
+            }
+            console.log(v + '->' +data);
+        }
     }
 }
-
-const Argentina = new Graph()
-Argentina.addNode("Buenos Aires")
-Argentina.addNode("Santa fe")
-Argentina.addNode("Córdoba")
-Argentina.addNode("Mendoza")
-Argentina.addNode("Kerala")
-Argentina.addNode("Malappuram")
-Argentina.addNode("Kozhikode")
-Argentina.addNode("Kannur")
-Argentina.addNode("Thrissur")
-Argentina.addNode("Kollam")
-Argentina.addConnection("Buenos Aires", "Córdoba")
-Argentina.addConnection("Buenos Aires", "Mendoza")
-Argentina.addConnection("Santa fe", "Córdoba")
-Argentina.addConnection("Santa fe", "Malappuram")
-Argentina.addConnection("Santa fe", "Kerala")
-Argentina.addConnection("Kerala", "Malappuram")
-Argentina.addConnection("Kerala", "Kozhikode")
-Argentina.addConnection("Kerala", "Kannur")
-Argentina.addConnection("Kerala", "Thrissur")
-Argentina.addConnection("Kerala", "Kollam")
-Argentina.removeNode('Kollam')
-
-console.log(Argentina)
-// Graph {
-//     adjacencyList: {
-//         'Buenos Aires': [ 'Córdoba', 'Mendoza' ],
-//         'Santa fe': [ 'Córdoba' ],
-//         'Córdoba': [ 'Buenos Aires', 'Santa fe' ],
-//         Mendoza: [ 'Buenos Aires' ]
-//     }
-// }
+const graph = new Graph();
+graph.addNode('adarsh')
+graph.addNode('shanu')
+graph.addNode('king')
+graph.addConnection('adarsh', 'kerala')
+graph.addConnection('abinesh', 'kerala')
+graph.addConnection('abinesh', 'king')
+graph.addConnection('king', 'adarsh')
+graph.addConnection('king', 'shanu')
+console.log(graph)
+graph.printGraph()
