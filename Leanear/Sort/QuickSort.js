ad1fe87044b064f0a331491af079526a6060a86c
Adarsh C <137256030+adarshshan@users.noreply.github.com>
2025-10-11 13:47:44 +0530

    Update QuickSort.js

--- Leanear/Sort/QuickSort.js
+++ Leanear/Sort/QuickSort.js
@@ -1,18 +1,4 @@
 //QUICK SORT
-// function quickSort(arr){
-//     if(arr.length<2)return arr;
-//     let pivot=arr[arr.length-1]
-//     let left=[];
-//     let right=[];
-//     for(let i=0;i<arr.length-1;i++){
-//         if(arr[i]<pivot){
-//             left.push(arr[i])
-//         }else{
-//             right.push(arr[i])
-//         }
-//     }
-//     return [...quickSort(left),pivot,...quickSort(right)];
-// }
 function QuickSort(arr) {
     if(arr.length<2)return arr;
     let left = [], right = [];
